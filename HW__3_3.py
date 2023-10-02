dadec99e39a630cee3fa7ac680508d374a552ebe
scott <scottjoon816@gmail.com>
2023-10-02 18:31:28 +0900

    M.WEEK3 HW3

--- HW__3_3.py
+++ HW__3_3.py
@@ -4,15 +4,14 @@ win = Tk()
 
 def add_task():
     task = U_entry.get()
-    if  task.strip() == 0:
-        ms.showerror("추가 버튼","값이 입력되지 않았습니다.")
-    else:
+    if len(task) > 0:
         taskLibox.insert(0, task)
         U_entry.delete(0,END)
 
 def del_task():
     task_selected = taskLibox.curselection()
-    taskLibox.delete(task_selected)       
+    if len(task_selected) > 0:
+        taskLibox.delete(task_selected)       
 
 notice = Label(win, text="Add a Task")
 U_entry = Entry(win)
