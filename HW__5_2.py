e77a7c724d533b6100b9614c1337900518b90fe2
= <scottjoon816@gmail.com>
2023-10-22 00:28:41 +0900

    WEEK5 HW2

--- HW__5_2.py
+++ HW__5_2.py
@@ -2,22 +2,19 @@ from collections import deque
 
 def solution(prices):
     results = deque(prices)
-    
-    # for index in range(len(prices)):
-    #     x = 0
-    #     for dy in range(index+1, len(prices)):
-    #         if prices[index] - prices[dy] <= 0:
-    #             x += 1
-    #     result.append(x)
     count = 0
+
     while count < len(prices):
         x = 0
+
         for price in prices[1:]:
             if results[0] <= price:
                 x += 1
+
         prices.pop(0)
         results.append(x)
         results.popleft()
+        
     return results
 
 if __name__ == '__main__':
